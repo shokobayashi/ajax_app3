25edc11319726bf893b06064fa089f8fce2ec6d7
shokobayashi <sho.kobayashi@di-v.co.jp>
2022-10-22 14:58:57 +0900

    firstcommit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+
+  # def new
+  # end
+
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
+end
